d2d3282045a4ff08bad4b87178360381dc29a3a8
jackhclee <38123458+jackhclee@users.noreply.github.com>
2022-06-05 21:26:00 +0100

    update psql code

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 app.get('/', async (req, res) => {
             //process.env.DATABASE_URL
   console.log(process.env.DATABASE_URL);
-  await client.connect();
   const result = await client.query('SELECT id, title from books')
   console.log(result.rows[0].id, result.rows[0].title) // Hello world!
   res.send(result.rows);
@@ -25,10 +24,17 @@ const client = new Client({
   }
 })
 
-app.listen(process.env.PORT || 3000, () => {
+let port = process.env.PORT || 3000;
 
-console.log('Started');
+app.listen(port, async () => {
 
+  console.log(`Express server started and listening at ${port}`);
+  await client.connect();
 }
 )
 
+process.on('SIGINT', () => {
+  console.log('exiting');
+  process.exit();
+}
+)

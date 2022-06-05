0530e9618d9a67ec29b06ca9d2a7d31cb59492a3
jackhclee <38123458+jackhclee@users.noreply.github.com>
2022-06-05 12:28:35 +0100

    init upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  res.send('Hello World');
+}
+
+)
+
+app.listen(443, () => {
+
+console.log('Started');
+
+}
+)
+

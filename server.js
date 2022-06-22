85073df6d682c00713314974e087bb2ea282ac64
jackhclee <38123458+jackhclee@users.noreply.github.com>
2022-06-22 21:57:43 +0100

    update cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const booksAPIPrefix = "books";
 
